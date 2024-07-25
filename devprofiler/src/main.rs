20d0627411770c5bda516a4a2c9c557ea26f0103
Muskan Paliwal <muskan10112002@gmail.com>
2024-07-25 13:46:37 +0530

    Update main.rs

--- devprofiler/src/main.rs
+++ devprofiler/src/main.rs
@@ -142,7 +142,7 @@ fn main() {
 					let rscanner = RepoScanner::new(scan_pathbuf);
 					let pathsvec = rscanner.scan(einfo, writer_mut, dockermode);
 					let alias_vec = process_repos(pathsvec, einfo, writer_mut, args.repo_slug, args.provider);
-					process_aliases(alias_vec, einfo, writer_mut, dockermode);
+					process_aliases(alias_vec, einfo, writer_mut, dockermode, wrapper);
 					let _res = einfo.write_runtime_info(writer_mut);
 					match writer.finish() {
 						Ok(_) => {
